b80659bfbed6c2cd9fa83a34c663838547644a28
donrestarone <shashikejayatunge@gmail.com>
2018-05-23 11:51:36 -0400

    start work on price sorter method

--- price-sorter-method.rb
+++ price-sorter-method.rb
@@ -1,4 +1,3 @@
-
 data = {
     "uber": [
         {
@@ -111,79 +110,12 @@ data = {
 }
 
 
+def price_sorter(data)
+	keys = data.keys
+	keys.each do |key|
+		data[key]
 
-
-
-uber_fare_types = [] # sorted arrays of options
-#p data[:uber].class
-
-data[:uber].each do |fare|
-  #p fare
-  uber_fares = Hash.new
-  uber_fares[:type] = fare[:type]
-  uber_fares[:fare] = fare[:fare]
-  #p uber_fares
-  uber_fare_types.push(uber_fares)
-  #fares["type"] = fare["type"]
-
-end
-#p fares[:type]
-fare_prices = []
-uber_fare_types.each do |fare|
-    if fare[:fare] != nil
-        fare_prices.push(fare[:fare])
-    end
-end
-fare_prices = fare_prices.sort{|x,y| x <=> y}
-
-
-sorted_prices_ridetypes_uber = []
-
-##sort the prices with their matching type and replace "POOL" with "UberPool"
-fare_prices.each do |price|
-    data[:uber].each do |fare|
-        
-        if fare[:fare] == price
-            if fare[:type] == "POOL"
-                fare[:type] = "UberPool"
-            end
-            
-            ride = Hash.new
-            ride[:type] = fare[:type]
-            ride[:price] = fare[:fare]
-            ride[:distance] = fare[:distance]
-            ride[:currency] = fare[:currency]
-            ride[:duration] = fare[:duration] 
-            
-            
-            #sorted_prices_ridetypes_uber.push(ride)
-            sorted_prices_ridetypes_uber.push(ride)
-        end
-    end 
-
-end 
-
-uber = Hash.new
-uber_fares = Array.new
-
-sorted_prices_ridetypes_uber.each do |fare|
-    #uber[:uber] = uber_fares
-    uber_fares.push(fare)
-
+	end
 end
 
-p uber_fares
-
-#puts sorted_prices_ridetypes_uber
-sorted_uber_lyft = {}
-##iterate through lyft
-data[:lyft].each do |fare|
-    
-end
-
-
-p data[:uber].count
-p data[:lyft].count
-
-
-
+p price_sorter(data)
